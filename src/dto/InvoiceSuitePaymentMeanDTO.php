f0ef1699021b9d7c0011ba6fe192dc55b580e2ac
HorstOeko <horstoeko@erling.com.de>
2025-09-25 11:25:16 +0200

    [DOC] Added file docs

--- src/dto/InvoiceSuitePaymentMeanDTO.php
+++ src/dto/InvoiceSuitePaymentMeanDTO.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is a part of horstoeko/invoicesuite.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace horstoeko\invoicesuite\dto;
 
 use horstoeko\invoicesuite\codelists\InvoiceSuiteCodelistPaymentMeans;
