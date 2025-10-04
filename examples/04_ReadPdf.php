f3017378c895c178c2f8ad5c0ae79e51a90d0f22
HorstOeko <horstoeko@erling.com.de>
2025-10-04 09:22:29 +0200

    [ENH] Improved example 04_ReadPdf

--- examples/04_ReadPdf.php
+++ examples/04_ReadPdf.php
@@ -10,3 +10,7 @@ $documentReader->getDocumentReader()->getDocumentNo($documentNo);
 $documentReader->getDocumentReader()->getDocumentDate($documentDate);
 echo $documentNo . PHP_EOL;
 echo $documentDate->format("d.m.Y") . PHP_EOL;
+echo count($documentReader->getAdditionalDocumentAttachments()) . " additional attachments found... \n";
+foreach($documentReader->getAdditionalDocumentAttachments() as $attachment) {
+    echo sprintf(" - %s (%s)\n", $attachment->getAttachmentFilename(), $attachment->getAttachmentMimeType());
+}
