b747f9ef132293067ca9f987f72f3203cc8c3b35
HorstOeko <horstoeko@erling.com.de>
2026-01-09 17:51:40 +0100

    [ENH] Enhanced prototype of Visualizer -> String Template Renderer

---
+++ src/visualizers/renderers/InvoiceSuiteVisualizerStringTemplateRenderer.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is a part of horstoeko/invoicesuite
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace horstoeko\invoicesuite\visualizers\renderers;
+
+use horstoeko\invoicesuite\documents\abstracts\InvoiceSuiteAbstractDocumentFormatReader;
+use horstoeko\invoicesuite\utils\InvoiceSuiteStringUtils;
+use horstoeko\invoicesuite\visualizers\abstracts\InvoiceSuiteVisualizerAbstractRenderer;
+
+/**
+ * Class representing a renderer which hold the content as a non-empty string
+ *
+ * @category InvoiceSuite
+ * @author   horstoeko <horstoeko@erling.com.de>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @see      https://github.com/horstoeko/invoicesuite
+ */
+class InvoiceSuiteVisualizerStringTemplateRenderer extends InvoiceSuiteVisualizerAbstractRenderer
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function templateExists(string $template): bool
+    {
+        return !InvoiceSuiteStringUtils::stringIsNullOrEmpty($template);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function render(InvoiceSuiteAbstractDocumentFormatReader $document, string $template): string
+    {
+        return $template;
+    }
+}
